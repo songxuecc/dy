ecdf29121fa6117f9d1847c7bb7f6d026f92bab8
mec0825 <guzetian@taofengcai.com>
2020-11-17 16:51:14 +0800

    [MOD] 跳转链接调整

--- src/pages/WodaInfo.vue
+++ src/pages/WodaInfo.vue
@@ -1,12 +1,12 @@
 <template lang="html">
     <div>
-      <a href="https://s.woda.com/8uJ3z" target="view_window">
+      <a href="https://fuwu.jinritemai.com/detail?service_id=19" target="view_window">
         <img src="https://dy-meizhe-woda.oss-cn-shanghai.aliyuncs.com/banner.png" style="width: 750px;">
       </a>
       <img src="https://dy-meizhe-woda.oss-cn-shanghai.aliyuncs.com/woda-1.png" style="width: 750px; margin-top: 20px;">
       <img src="https://dy-meizhe-woda.oss-cn-shanghai.aliyuncs.com/woda-2.png" style="width: 750px;">
       <img src="https://dy-meizhe-woda.oss-cn-shanghai.aliyuncs.com/woda-3.png" style="width: 750px;">
-      <a href="https://s.woda.com/8uJ3z" target="view_window">
+      <a href="https://fuwu.jinritemai.com/detail?service_id=19" target="view_window">
         <img src="https://dy-meizhe-woda.oss-cn-shanghai.aliyuncs.com/banner.png" style="width: 750px;">
       </a>
     </div>
