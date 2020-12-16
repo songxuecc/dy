eb4ab384faa76171ca6b2335fc2e00f0ed9f8feb
delu <luchaoyang@meideng.net>
2020-12-16 11:31:43 +0800

    feature(float-view): 右侧浮动框增加反馈按钮，点击跳转问卷

---
+++ src/components/HhIcon/index.vue
@@ -0,0 +1,10 @@
+<script>
+import createFromIconfontCN from './createFromIconfontCN.js'
+
+const IconFont = createFromIconfontCN([
+  '//at.alicdn.com/t/font_1334237_mbgkamj55j.js'
+])
+
+export default IconFont
+
+</script>
