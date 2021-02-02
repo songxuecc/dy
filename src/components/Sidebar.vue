5cf931ef4caa3dffcf0afd9836fd948f04c1c94b
songxue <songxue@meideng.net>
2021-02-02 18:31:37 +0800

    feat: 新功能提示按钮

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -15,7 +15,7 @@
               <span slot="title">准备搬家</span>
             </el-menu-item>
             <el-menu-item index="/migrateSetting" :disabled="!isAuth()">
-              <span slot="title" style="display: flex;align-items: center;">搬家设置 <hh-icon type="icon60" class="left-translate"><span class="font-12 new-features" style="">新功能</span> </hh-icon></span>
+              <span slot="title" style="display: flex;align-items: center;">搬家设置<NewFeatureTips /></span>
             </el-menu-item>
           </el-submenu>
           <el-submenu index="2">
@@ -27,7 +27,7 @@
               <span slot="title">全部商品</span>
             </el-menu-item>
             <el-menu-item index="/batchEdit/title" :disabled="!isAuth()">
-              <span slot="title">批量处理</span>
+              <span slot="title" style="display: flex;align-items: center;">批量处理<NewFeatureTips /></span>
             </el-menu-item>
             <el-menu-item index="/poster/dg/list" :disabled="!isAuth()">
               <span slot="title">详情海报</span>
@@ -53,12 +53,14 @@
 <script>
 import {mapGetters} from 'vuex'
 import common from '@/common/common.js'
+import NewFeatureTips from '@/components/NewFeatureTips.vue'
 
 export default {
   data () {
     return {}
   },
   inject: ['reload'],
+  components: {NewFeatureTips},
   methods: {
     ...mapGetters({
       isAuth: 'getIsAuth',
@@ -83,45 +85,5 @@ export default {
 
 <style lang="less" scoped>
   @import '~@/assets/css/sidebar.less';
-  /deep/ .left-translate {
-    margin-left:-2px;
-    color:#ffffff;
-    font-size:16px;
-    padding:0 2px !important;
-    line-height:14px;
-    border-radius:2px;
-    animation: slideLeft 1s infinite linear;
-    @keyframes slideLeft {
-      0% {
-        transform: translate(0px);
-      }
-
-      25% {
-        transform: translate(1px);
-      }
-
-      50% {
-        transform: translate(3px);
-      }
-
-      75% {
-        transform: translate(1px);
-      }
-
-      100% {
-        transform: translate(0);
-      }
-    }
-  }
-
-  .new-features {
-    padding:0 !important;
-    font-size:12px;
-    color:#d81e06;
-    transform:scale(0.8);
-    display:inline-block;
-    margin-left: -6px;
-    padding-top: 2px !important;
-  }
 
 </style>
