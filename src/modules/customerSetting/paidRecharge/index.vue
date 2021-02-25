6195c7efffd483c7f88fb38a728a8b3ae91b0f7d
songxue <songxue@meideng.net>
2021-02-25 18:59:09 +0800

    feat: 额度规则计算modal

--- src/modules/customerSetting/paidRecharge/index.vue
+++ src/modules/customerSetting/paidRecharge/index.vue
@@ -7,6 +7,15 @@
         </el-radio-group>
         <PayRecord v-if="tabType === 'PayRecord' "/>
         <Recharge v-if="tabType === 'Recharge' " />
+        <el-dialog title="额度计算规则" class="product-dialog" :visible.sync="vislble" @close="dialogEditClose" v-hh-modal  width="400px" >
+            <div>
+                <p>1、订购免费版赠送10条额度，订购1个月版赠送100条额度，订购3个月版赠送300条额度，订购了6个月版赠送600条额度，订购12个月版赠送1200条额度；</p>
+                <p>2、可通过付费充值增加额度，收费标准：100条=10元、300条=25元、500条=35元、1000条=60元；</p>
+                <p>3、赠送额度、充值额度均可累计且在任何场景下不会被清空；</p>
+                <p>4、抓取成功耗费一条额度，抓取失败不耗费额度。</p>
+            </div>
+            <p class="flex justify-c"><el-button type="primary" style="width:120px">我知道了</el-button></p>
+        </el-dialog>
     </div>
 </template>
 
@@ -16,7 +25,8 @@ import Recharge from './Recharge.vue'
 export default {
   data () {
     return {
-      tabType: 'PayRecord'
+      tabType: 'PayRecord',
+      vislble: false
     }
   },
   components: {
