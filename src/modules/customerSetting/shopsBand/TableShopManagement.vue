e83a7550d0409cd94409a63a040b0fab6a852034
songxue <songxue@meideng.net>
2021-02-25 16:34:30 +0800

    feat: 修改样式

--- src/modules/customerSetting/shopsBand/TableShopManagement.vue
+++ src/modules/customerSetting/shopsBand/TableShopManagement.vue
@@ -25,16 +25,14 @@
       <el-table-column
         prop="auth_status"
         label="授权状态"
-        width="180">
+        width="220">
         <span slot-scope="scope" :class="['expire', 'un_login'].includes(scope.row.auth_status) ? 'expire':'auth'">
           <span v-if="scope.row.auth_status === 'auth'">授权中</span>
           <span v-if="scope.row.auth_status === 'un_login'">登录过期
             <el-link
-            type="primary"
             style="display:inline;font-size:12px;margin-left:5px"
-            @click="goLogin"
             :underline="false"
-            >去登录></el-link>
+            >请用该店铺账号登录虎虎</el-link>
           </span>
           <span v-if="scope.row.auth_status === 'expire'">订购过期
             <el-link
@@ -103,10 +101,6 @@ export default {
     },
     openDinggou () {
       window.open('https://fuwu.jinritemai.com/detail?from=fuwu_market_home&service_id=42')
-      return false
-    },
-    goLogin () {
-      window.open('https://fxg.jinritemai.com/login')
       return false
     }
   },
