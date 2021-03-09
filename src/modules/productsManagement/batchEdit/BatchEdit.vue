36682d4887e62370bad4525b5c9a75b889401b55
songxue <songxue@meideng.net>
2021-03-09 11:30:08 +0800

    feat: 上下架暂时不上线

--- src/modules/productsManagement/batchEdit/BatchEdit.vue
+++ src/modules/productsManagement/batchEdit/BatchEdit.vue
@@ -8,7 +8,7 @@
 <!--      <help-tips :helpLink="activeTabName" positionT="10" positionR="400"></help-tips>-->
       <el-col :span="14" style="text-align: left; padding-left: 10px;">
         <el-tabs v-model="activeTabName" @tab-click="handleTabClick" :before-leave="beforeLeaveTab">
-         <el-tab-pane label="上下架" name="saleStatus"></el-tab-pane>
+         <!-- <el-tab-pane label="上下架" name="saleStatus"></el-tab-pane> -->
           <el-tab-pane label="改标题" name="title"></el-tab-pane>
 <!--          <el-tab-pane label="改描述" name="description"></el-tab-pane>-->
 <!--          <el-tab-pane label="改类目" name="category"></el-tab-pane>-->
@@ -192,7 +192,7 @@ export default {
   },
   data () {
     return {
-      activeTabName: 'saleStatus',
+      activeTabName: 'title',
       dyProductModelList: [],
       selectProductList: [],
       selectProductDict: {},
@@ -296,7 +296,7 @@ export default {
           if (data.items[i].goods_id in this.selectProductDict) {
             product = this.selectProductDict[data.items[i].goods_id]
           } else {
-            data.items[i].is_onsale = (data.items[i].status === 2 ? 0 : 1)
+            // data.items[i].is_onsale = (data.items[i].status === 2 ? 0 : 1)
             for (let j in data.items[i].sku_list) {
               data.items[i].sku_list[j].price /= 100.0
             }
