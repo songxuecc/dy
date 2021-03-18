4cb09d17412134e72a4bc979f38489d80bd56603
songxue <songxue@meideng.net>
2021-03-18 17:24:04 +0800

    feat: 修改拖拽元素

--- src/directives/hh-drag.js
+++ src/directives/hh-drag.js
@@ -1,14 +1,13 @@
 // 这个助手方法下面会用到，用来获取 css 相关属性值
 
-import debounce from 'lodash/debounce'
 const getAttr = (obj, key) => (
   obj.currentStyle
     ? obj.currentStyle[key]
     : window.getComputedStyle(obj, false)[key]
 )
 
-let right = 0
-let top = 0
+let right = 15
+let top = parseInt(getAttr(document.body, 'height')) / 2
 let minRight = 0
 let maxRight = 0
 let minTop = 0
@@ -16,6 +15,7 @@ let maxTop = 0
 let currentX = 0
 let currentY = 0
 let unbindEvents = () => {}
+let isDown = false
 const hhDrag = {
   inserted (el, binding, vnode) {
     /**
@@ -32,9 +32,11 @@ const hhDrag = {
     }
     el.onmousedown = (e) => {
       change()
+      document.body.style.cssText += 'user-select: none;-webkit-user-select:none;-moz-user-select:none;-ms-user-select: none'
+
+      isDown = true
       currentX = e.clientX
       currentY = e.clientY
-      document.body.style.cssText += 'user-select: none;-webkit-user-select:none;-moz-user-select:none;-ms-user-select: none'
       right = parseInt(getAttr(target, 'right'))
       top = parseInt(getAttr(target, 'top'))
       minRight = parseInt(getAttr(document.body, 'width')) - (target.offsetLeft + parseInt(getAttr(target, 'width'))) - parentRight
@@ -46,51 +48,53 @@ const hhDrag = {
     }
 
     const bindEvents = () => {
-      document.addEventListener('mouseup', onMouseUp)
       document.addEventListener('mousemove', onMouseMove)
-      document.addEventListener('mouseleave', onMouseUp)
     }
+
+    document.addEventListener('mouseup', function (e) {
+      isDown = false
+      unbindEvents()
+    })
+
     unbindEvents = () => {
       console.log('unbindEvents')
-      right = 0
-      top = 0
+      document.removeEventListener('mousemove', onMouseMove)
+      right = 15
+      top = parseInt(getAttr(document.body, 'height')) / 2
       minRight = 0
       maxRight = 0
       minTop = 0
       maxTop = 0
       currentX = 0
       currentY = 0
-      document.removeEventListener('mouseup', onMouseUp)
-      document.removeEventListener('mousemove', onMouseMove)
-      document.removeEventListener('mouseleave', onMouseUp)
       change()
       document.body.style.cssText += 'user-select: text;-webkit-user-select:text;-moz-user-select:text;-ms-user-select: text'
     }
-    const onMouseUp = event => {
-      unbindEvents()
-    }
-    const onMouseMove = debounce(function (event) {
+
+    const onMouseMove = function (event) {
+      if (isDown) {
       // 鼠标移动时计算每次移动的距离，并改变拖拽元素的定位
-      const disX = event.clientX - currentX
-      const disY = event.clientY - currentY
-      if (disX < 0 && disX - right <= -maxRight) { // 向左移动
-        target.style.right = `${maxRight}px`
-      } else if (disX > 0 && disX > minRight) { // 向右移动
-        target.style.right = `${parentRight}px`
-      } else {
-        target.style.right = `${right - disX}px`
-      }
+        const disX = event.clientX - currentX
+        const disY = event.clientY - currentY
+        if (disX < 0 && disX - right <= -maxRight) { // 向左移动
+          target.style.right = `${maxRight}px`
+        } else if (disX > 0 && disX > minRight) { // 向右移动
+          target.style.right = `${parentRight}px`
+        } else {
+          target.style.right = `${right - disX}px`
+        }
 
       // 判断上、下边界
-      if (disY < 0 && disY <= -minTop) {
-        target.style.top = `${top - minTop}px`
-      } else if (disY > 0 && Math.abs(disY) >= maxTop) {
-        target.style.top = `${top + maxTop}px`
-      } else {
-        target.style.top = `${top + disY}px`
+        if (disY < 0 && disY <= -minTop) {
+          target.style.top = `${top - minTop}px`
+        } else if (disY > 0 && Math.abs(disY) >= maxTop) {
+          target.style.top = `${top + maxTop}px`
+        } else {
+          target.style.top = `${top + disY}px`
+        }
+        return false
       }
-      return false
-    }, 0)
+    }
   },
 
   update (el) {
