693a888a1367c24d80dcad271de2c9ccae598390
songxue <songxue@meideng.net>
2021-03-29 18:48:51 +0800

    reacfor: 单个sku的修改modal

---
+++ src/modules/migrate/migrateSettingPrice/ModalSingleSkuList.vue
@@ -0,0 +1,19 @@
+<!--  -->
+<template>
+    <div></div>
+</template>
+
+<script>
+export default {
+  name: 'ModalSingleSkuList',
+  props: {
+    msg: String
+  },
+  data () {
+    return {
+    }
+  }
+}
+</script>
+<style lang="less" scoped>
+</style>
