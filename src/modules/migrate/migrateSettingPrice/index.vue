54a672a3ee5482e0459b9bbdadd5949945b95e40
songxue <songxue@meideng.net>
2021-04-09 19:00:11 +0800

    fix: 价格页面首次loading

--- src/modules/migrate/migrateSettingPrice/index.vue
+++ src/modules/migrate/migrateSettingPrice/index.vue
@@ -3,13 +3,13 @@
   <div >
     <Title class="mt-5 mb-5" />
     <TableSkuPriceList ref="tableSkuPriceList" @templateError="handleTemplateError"
-      v-loading="loadingGetTPProductByIds" />
+      v-loading="loadingData" />
     <div class="common-bottom flex justify-c">
-      <el-button style="margin-right: 15px; width:120px" @click="goback" :disabled="loadingGetTPProductByIds" plain
-        type="primary" :loading="loadingGetTPProductByIds">返回</el-button>
+      <el-button style="margin-right: 15px; width:120px" @click="goback" :disabled="loadingData" plain
+        type="primary" :loading="loadingData">返回</el-button>
       <div>
-        <el-button type="primary" :disabled="msgError || loadingGetTPProductByIds" @click="toSetTemplate"
-          :loading="loadingGetTPProductByIds">下一步：修改模板</el-button>
+        <el-button type="primary" :disabled="msgError || loadingData" @click="toSetTemplate"
+          :loading="loadingData">下一步：修改模板</el-button>
         <NewComer type="下一步：修改模板" ref="newComer">
           <div class="left">
             <div style="width:180px" class="color-666 font-12">点击此处进行下一步操作</div>
@@ -40,16 +40,20 @@ export default {
     NewComer
   },
   computed: {
-    ...mapState('migrate/migrateSettingPrice', ['tableData', 'originTableData']),
+    ...mapState('migrate/migrateSettingPrice', ['tableData', 'originTableData', 'origin']),
     ...mapGetters('migrate/migrateSettingTemplate', {
       template: 'getTemplate'
     }),
     ...mapGetters('migrate/migrateSettingPrice', ['tableDataErrorMsg', 'dicCustomPrices', 'customPrices']),
     ...mapState({
-      loadingGetTPProductByIds: state => state['@@loading'].effects['migrate/migrateSettingPrice/getTPProductByIds']
+      loadingGetTPProductByIds: state => state['@@loading'].effects['migrate/migrateSettingPrice/getTPProductByIds'],
+      loadingFormatTableData: state => state['@@loading'].effects['migrate/migrateSettingPrice/formatTableData']
     }),
     msgError () {
       return this.tableDataErrorMsg.some(item => !isEmpty(item)) || this.templateError
+    },
+    loadingData () {
+      return this.loadingGetTPProductByIds || this.loadingFormatTableData
     }
   },
   activated () {
