07435f07e7cf64061c7341d91ff653778b6ba975
songxue <songxue@meideng.net>
2021-05-25 13:53:22 +0800

    feat: 选择商品

---
+++ src/modules/productsManagement/batchEdit/components/selectProduct.vue
@@ -0,0 +1,19 @@
+<!-- 选择商品 -->
+<template>
+    <div></div>
+</template>
+
+<script>
+export default {
+  name: 'selectProduct',
+  props: {
+    msg: String
+  },
+  data () {
+    return {
+    }
+  }
+}
+</script>
+<style lang="less" scoped>
+</style>
