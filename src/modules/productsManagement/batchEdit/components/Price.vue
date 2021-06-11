a02d41397ad72efcc13513ba7ff04c959bb1d940
songxue <songxue@meideng.net>
2021-06-11 15:22:20 +0800

    feat: 价格设置

--- src/modules/productsManagement/batchEdit/components/Price.vue
+++ src/modules/productsManagement/batchEdit/components/Price.vue
@@ -5,25 +5,34 @@
       <el-form-item>
         <el-radio v-model="form.is_formula" @change="handleIsFormulaChange" :label="1">
           <span class="color-4e font-12">按照公式修改&nbsp;原价*</span>
-          <el-input-number
+          <el-input
             style="width: 80px"
             :max="99999999"
             v-model="form.origin_price_rate"
             @focus="handleIsFormulaFocus"
+            clearable
+            @clear="handleClear('origin_price_rate')"
+            @blur="handleBlur($event,'origin_price_rate')"
           />
           <span class="color-4e font-12">&nbsp;%&nbsp;+&nbsp;</span>
-          <el-input-number
+          <el-input
             style="width: 80px"
             :max="99999999"
             v-model="form.incr_diff"
             @focus="handleIsFormulaFocus"
+            clearable
+            @clear="handleClear('incr_diff')"
+            @blur="handleBlur($event,'incr_diff')"
           />
           <span class="color-4e font-12">&nbsp;-&nbsp;</span>
-          <el-input-number
+          <el-input
             style="width: 80px"
             :max="99999999"
             v-model="form.desc_diff"
             @focus="handleIsFormulaFocus"
+            clearable
+            @clear="handleClear('desc_diff')"
+            @blur="handleBlur($event,'desc_diff')"
           />
         </el-radio>
       </el-form-item>
@@ -32,13 +41,17 @@
           v-model="form.is_every_price"
           @change="handleIsEveryPriceChange"
           :label="1"
+
         >
           <span class="color-4e font-12">价格统一为&nbsp;</span>
-          <el-input-number
+          <el-input
             style="width: 330px"
             v-model="form.every_price"
             :max="99999999"
             @focus="handleIsEveryPriceFocus"
+            clearable
+            @clear="handleClear('every_price')"
+            @blur="handleBlur($event,'every_price')"
           />
         </el-radio>
       </el-form-item>
@@ -50,10 +63,14 @@
               content="若此处填10，当新价格低于10元，则将新价格调整为10"
               placement="right"
             >
-              <el-input-number
+              <el-input
                 :max="99999999"
                 style="width: 230px"
                 v-model="form.min_price"
+                @focus="handlChecked('is_open_min_price')"
+                clearable
+                @clear="handleClear('min_price')"
+                @blur="handleBlur($event,'min_price')"
               />
             </el-tooltip>
           </el-form-item>
@@ -67,10 +84,14 @@
               content="若此处填200，当新价格低于原价的2倍时，则将新价格调整为原价的2倍"
               placement="right"
             >
-              <el-input-number
+              <el-input
                 :max="99999999"
                 style="width: 194px"
+                @focus="handlChecked('is_open_min_price_rate')"
                 v-model="form.min_price_rate"
+                clearable
+                @clear="handleClear('min_price_rate')"
+                @blur="handleBlur($event,'min_price_rate')"
               />
             </el-tooltip>
             <span class="color-4e font-12">&nbsp;%&nbsp;</span>
@@ -101,9 +122,9 @@ export default {
         desc_diff: undefined,
         is_every_price: 0,
         every_price: undefined,
-        is_open_min_price: 0,
+        is_open_min_price: false,
         min_price: undefined,
-        is_open_min_price_rate: 0,
+        is_open_min_price_rate: false,
         min_price_rate: undefined
       }
     }
@@ -118,6 +139,7 @@ export default {
     },
     handleIsFormulaFocus () {
       this.form.is_formula = 1
+      this.form.is_every_price = 0
     },
     handleIsEveryPriceChange (value) {
       if (this.form.is_every_price) {
@@ -127,9 +149,20 @@ export default {
       }
     },
     handleIsEveryPriceFocus () {
+      this.form.is_formula = 0
       this.form.is_every_price = 1
     },
+    handlChecked (attribute) {
+      this.form[attribute] = true
+    },
+    handleClear (attribute) {
+      this.form[attribute] = ''
+    },
+    handleBlur (e, attribute) {
+      this.form[attribute] = parseInt(e.target.value) ? parseInt(e.target.value) : ''
+    },
     getForm () {
+      console.log(this.form, 'this.form')
       return {
         ...this.form,
         is_formula: Number(this.form.is_formula),
--- src/modules/productsManagement/batchEdit/components/Stocks.vue
+++ src/modules/productsManagement/batchEdit/components/Stocks.vue
@@ -11,11 +11,13 @@
           <p class="font-12 color-4e mb-10">若商品是现货或预售发货模式</p>
           <el-radio :label="1">
             <el-form-item label-width="80px" class="block" label="库存统一增加">
-              <el-input-number
+              <el-input
                 controls-position="right"
                 v-model="form.stock_value_1"
                 @focus="handleCheck(1)"
+                clearable
                 @clear="handleClear('stock_value_1')"
+                @blur="handleBlur($event,'stock_value_1')"
                 :picker-options="pickerOptions"
                 :max="999999"
                 class="input-number"
@@ -24,11 +26,13 @@
           </el-radio>
           <el-radio :label="2">
             <el-form-item label-width="80px" class="block" label="库存统一减少">
-              <el-input-number
+              <el-input
                 controls-position="right"
                 v-model="form.stock_value_2"
                 @focus="handleCheck(2)"
+                clearable
                 @clear="handleClear('stock_value_2')"
+                @blur="handleBlur($event,'stock_value_2')"
                 :picker-options="pickerOptions"
                 :max="999999"
                 class="input-number"
@@ -37,11 +41,13 @@
           </el-radio>
           <el-radio :label="3">
             <el-form-item label-width="80px" class="block" label="库存统一为">
-              <el-input-number
+              <el-input
                 controls-position="right"
                 v-model="form.stock_value_3"
                 @focus="handleCheck(3)"
+                clearable
                 @clear="handleClear('stock_value_3')"
+                @blur="handleBlur($event,'stock_value_3')"
                 :picker-options="pickerOptions"
                 :max="999999"
                 class="input-number"
@@ -60,11 +66,13 @@
                   class="block"
                   label="库存统一增加"
                 >
-                  <el-input-number
+                  <el-input
                     controls-position="right"
                     v-model="form.stock_value_4"
                     @focus="handleCheck(4)"
+                    clearable
                     @clear="handleClear('stock_value_4')"
+                    @blur="handleBlur($event,'stock_value_4')"
                     :picker-options="pickerOptions"
                     :max="999999"
                     class="input-number"
@@ -77,11 +85,13 @@
                   class="block"
                   label="库存统一减少"
                 >
-                  <el-input-number
+                  <el-input
                     controls-position="right"
                     v-model="form.stock_value_5"
                     @focus="handleCheck(5)"
+                    clearable
                     @clear="handleClear('stock_value_5')"
+                    @blur="handleBlur($event,'stock_value_5')"
                     :picker-options="pickerOptions"
                     :max="999999"
                     class="input-number"
@@ -94,11 +104,13 @@
                   class="block"
                   label="库存统一为"
                 >
-                  <el-input-number
+                  <el-input
                     controls-position="right"
                     v-model="form.stock_value_6"
                     @focus="handleCheck(6)"
+                    clearable
                     @clear="handleClear('stock_value_6')"
+                    @blur="handleBlur($event,'stock_value_6')"
                     :picker-options="pickerOptions"
                     :max="999999"
                     class="input-number"
@@ -114,11 +126,13 @@
                   class="block"
                   label="阶梯库存统一增加"
                 >
-                  <el-input-number
+                  <el-input
                     controls-position="right"
                     v-model="form.stock_value_7"
                     @focus="handleCheck(7)"
+                    clearable
                     @clear="handleClear('stock_value_7')"
+                    @blur="handleBlur($event,'stock_value_7')"
                     :picker-options="pickerOptions"
                     :max="999999"
                     class="input-number"
@@ -131,11 +145,13 @@
                   class="block"
                   label="阶梯库存统一减少"
                 >
-                  <el-input-number
+                  <el-input
                     controls-position="right"
                     v-model="form.stock_value_8"
                     @focus="handleCheck(8)"
+                    clearable
                     @clear="handleClear('stock_value_8')"
+                    @blur="handleBlur($event,'stock_value_8')"
                     :picker-options="pickerOptions"
                     :max="999999"
                     class="input-number"
@@ -148,11 +164,13 @@
                   class="block"
                   label="阶梯库存统一为"
                 >
-                  <el-input-number
+                  <el-input
                     controls-position="right"
                     v-model="form.stock_value_9"
                     @focus="handleCheck(9)"
+                    clearable
                     @clear="handleClear('stock_value_9')"
+                    @blur="handleBlur($event,'stock_value_9')"
                     :picker-options="pickerOptions"
                     :max="999999"
                     class="input-number"
@@ -196,6 +214,9 @@ export default {
     },
     handleClear (attribute) {
       this.form[attribute] = ''
+    },
+    handleBlur (e, attribute) {
+      this.form[attribute] = parseInt(e.target.value) ? parseInt(e.target.value) : ''
     },
     getForm () {
       return {
