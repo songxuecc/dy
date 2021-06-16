1b0a0fbab0a9602b30b90e47b959149c6cbc1df3
songxue <songxue@meideng.net>
2021-06-16 17:49:33 +0800

    fix: 添加热门标签

--- src/components/HhIcon/index.vue
+++ src/components/HhIcon/index.vue
@@ -2,7 +2,7 @@
 import createFromIconfontCN from './createFromIconfontCN.js'
 
 const IconFont = createFromIconfontCN([
-  '//at.alicdn.com/t/font_2290850_dqickbijnnb.js'
+  '//at.alicdn.com/t/font_2290850_1ogn0sbly5t.js'
 ])
 
 export default IconFont
