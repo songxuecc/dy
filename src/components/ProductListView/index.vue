f60b5d17723c59d5297652870c59bddae2ad1cc5
delu <luchaoyang@meideng.net>
2021-07-22 10:27:35 +0800

    mod(搬家文案): 文案调整

--- src/components/ProductListView/index.vue
+++ src/components/ProductListView/index.vue
@@ -201,7 +201,7 @@
                               请刷新品牌后重新搬家
                             </ul>
                         </div>
-                        <div slot="content"  v-else-if="scope.row.migration_msg[0].indexOf('店铺未授权该类目，请先去抖店后台开通') > -1 && scope.row.status === 5"  >
+                        <div slot="content"  v-else-if="scope.row.migration_msg[0].indexOf('店铺未授权该类目') > -1 && scope.row.status === 5"  >
                             <ul style="padding: 0; margin: 0; margin-top: 6px;" :key="0"  class="font-13">
                               <hh-icon type="iconjinggao1"></hh-icon>
                               若店铺未授权当前类目，请先去抖店后台检查。若已开通，则重新刷新类目后再搬家
