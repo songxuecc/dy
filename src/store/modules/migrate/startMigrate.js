1b4e3e54740a99436e05ce117fa5b0ffc6d89f91
songxue <songxue@meideng.net>
2021-08-13 18:20:27 +0800

    feat: 弹出提示

--- src/store/modules/migrate/startMigrate.js
+++ src/store/modules/migrate/startMigrate.js
@@ -30,6 +30,32 @@ export default assign(tableData, {
         console.log(err)
         this._vm.$message.error(`${err}`)
       }
+    },
+    async getCaptureShopCompleteList ({commit, state, dispatch}, payload) {
+      try {
+        const data = await Api.hhgjAPIs.getCaptureShopCompleteList()
+        if (data.length) {
+          let msg = ''
+          data.forEach(item => {
+            msg += `<div>恭喜，抖音平台【${item.shop_name}】店铺已全部抓取完毕，您可以前往进行下一步操作</div>`
+          })
+          this._vm.$message({
+            type: 'success',
+            dangerouslyUseHTMLString: true,
+            message: msg
+          })
+        } else {
+          const timer = setTimeout(() => {
+            clearTimeout(state.timer)
+            commit('save', {timer: null})
+            dispatch('getCaptureShopCompleteList')
+          }, 10000)
+          commit('save', {timer})
+        }
+      } catch (err) {
+        console.log(err)
+        this._vm.$message.error(`${err}`)
+      }
     }
   },
   getters: {
