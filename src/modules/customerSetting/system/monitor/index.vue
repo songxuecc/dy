4a3bb7e88a714f36ebbc7ff84a87d85260534c5d
delu <luchaoyang@meideng.net>
2021-08-16 15:49:15 +0800

    mod(监控): 移除重启队列操作交互

--- src/modules/customerSetting/system/monitor/index.vue
+++ src/modules/customerSetting/system/monitor/index.vue
@@ -1,23 +1,23 @@
 <template>
   <div style="text-align: left;">
     <el-container>
-      <el-header>
-        <el-button @click="doCmd('all')">
-          重启所有队列
-        </el-button>
-        <el-button @click="doCmd('django_code')">
-          重启django-web
-        </el-button>
-        <el-button @click="doCmd('fe')">
-          重启前端fe
-        </el-button>
-      </el-header>
+<!--      <el-header>-->
+<!--        <el-button @click="doCmd('all')">-->
+<!--          重启所有队列-->
+<!--        </el-button>-->
+<!--        <el-button @click="doCmd('django_code')">-->
+<!--          重启django-web-->
+<!--        </el-button>-->
+<!--        <el-button @click="doCmd('fe')">-->
+<!--          重启前端fe-->
+<!--        </el-button>-->
+<!--      </el-header>-->
       <el-main>
         <div style="display: inline-block;" v-for="(item, index) in queueList" :key="index">
           <el-card class="box-card" style="width: 320px; height:300px">
             <div slot="header" class="clearfix">
               <span>{{item.queue_group_name}}</span>
-              <el-button style="float: right; padding: 3px 0" type="text" @click="doCmd(item.queue_group_name)">重启队列</el-button>
+<!--              <el-button style="float: right; padding: 3px 0" type="text" @click="doCmd(item.queue_group_name)">重启队列</el-button>-->
             </div>
             <div v-for="(queue_item, index) in item.queue_item_list" :key="index" class="text item">
               {{ queue_item.queue + ': ' + queue_item.task_nums }}
