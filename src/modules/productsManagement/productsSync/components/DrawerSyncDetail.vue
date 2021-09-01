c060661bf059087342d3028dbcc60d7315dbb55c
songxue <songxue@meideng.net>
2021-09-01 17:55:16 +0800

    feat: ui全部完成

--- src/modules/productsManagement/productsSync/components/DrawerSyncDetail.vue
+++ src/modules/productsManagement/productsSync/components/DrawerSyncDetail.vue
@@ -7,6 +7,8 @@
     :with-header="false"
     :append-to-body="true"
   >
+    <ModalDataCompared ref="ModalDataCompared" />
+
     <div class="pl-30 pr-30">
       <el-tabs v-model="activeName">
         <el-tab-pane
@@ -17,43 +19,30 @@
         >
           <div>
             <el-table
-              :data="hhTaskProductPageTableData"
+              :data="tableData"
               v-loading="loading"
               row-key="goods_id"
               height="calc(100vh - 225px)"
               style="width: 100%; box-sizing: border-box"
             >
               <el-table-empty slot="empty" />
-              <el-table-column
-                prop="image_url"
-                label="商品图片"
-                align="center"
-              >
-                <template slot-scope="scope">
-                  <img
-                    :src="scope.row.image_url"
-                    style="height: 50px; max-width: 65px"
-                  />
-                </template>
+              <el-table-column prop="task_title" label="计划名称" >
               </el-table-column>
-              <el-table-column prop="goods_name" label="商品信息" >
+              <el-table-column prop="task_title" label="计划类型" >
               </el-table-column>
-              <el-table-column align="center" label="分类" prop="category_name" width="100">
+              <el-table-column prop="success_nums" label="商品数">
               </el-table-column>
-              <el-table-column
-                prop="old_data"
-                label="修改前"
-                align="center"
-              >
+              <el-table-column prop="date" label="同步结果" >
+              </el-table-column>
+              <el-table-column prop="create_time" label="状态" >
               </el-table-column>
 
-              <el-table-column width="100" align="center" label="操作">
+              <el-table-column width="300" label="操作">
                 <template slot-scope="scope">
-                  <a
-                    class="font-12 pointer primary mr-5"
-                    @click="handleView(scope.$index, scope.row)"
-                    >前往后台</a
-                  >
+                  <a class="pramiry pointer " @click="handleSync(scope.row)">发布到抖音</a>
+                  <a class="pramiry pointer pl-5" @click="handleDetail(scope.row)">查看详情</a>
+                  <a class="pramiry pointer pl-5" @click="handleRevert(scope.row)">还原</a>
+                  <a class="fail pointer pl-5" @click="on(scope.row)">重新同步</a>
                 </template>
               </el-table-column>
             </el-table>
@@ -64,7 +53,7 @@
               @current-change="handleCurrentChange"
               :current-page="pagination.page_index"
               :page-size="pagination.page_size"
-              :page-sizes="hhTaskProductPageSizes"
+              :page-sizes="[10, 20, 50, 100]"
               @size-change="handleSizeChange"
               :total="hhTaskProductPageTotal"
             >
@@ -72,17 +61,20 @@
           </div>
         </el-tab-pane>
       </el-tabs>
+
     </div>
   </el-drawer>
 </template>
 
 <script>
 import {mapActions, mapState} from 'vuex'
-// import debounce from 'lodash/debounce'
-
+import ModalDataCompared from './ModalDataCompared'
 export default {
   name: 'DrawerSyncDetail',
   props: {
+  },
+  components: {
+    ModalDataCompared
   },
   data () {
     return {
@@ -148,9 +140,100 @@ export default {
         'https://fxg.jinritemai.com/index.html#/ffa/g/create?product_id=' +
           product.goods_id
       )
+    },
+    handleDetail () {
+      this.$refs.ModalDataCompared && this.$refs.ModalDataCompared.open()
+    },
+    handleSync () {
+      console.log(this, 'this')
+      const h = this.$createElement
+      this.$confirm('', {
+        message: h('div', null, [
+          h('div', {
+            class: 'center'
+          }, [
+            h('hh-icon', {
+              props: {
+                type: 'iconjinggao1'
+              },
+              class: 'TableSyncRecord-icon'
+            })
+          ]),
+          h('div', {
+            class: 'TableSyncRecord-text'
+          }, '您所选中的同步数据将更新到抖店，您确定要操作吗？?')
+        ]),
+        type: 'warning',
+        customClass: 'TableSyncRecord-customClass',
+        cancelButtonClass: 'TableSyncRecord-cancelButtonClass',
+        confirmButtonClass: 'TableSyncRecord-confirmButtonClass',
+        showClose: false
+      })
+        .then(_ => {
+          console.log('00000')
+        })
+        .catch(_ => {
+          return false
+        })
     }
   }
 }
 </script>
 <style lang="less" scoped>
+</style>
+<style lang="less">
+.TableSyncRecord-cancelButtonClass{
+  font-size: 12px;
+  margin-right: 10px;
+  width: 120px;
+  padding: 12px;
+}
+
+.TableSyncRecord-confirmButtonClass{
+  font-size: 12px;
+  width: 120px;
+  padding: 12px;
+}
+
+.TableSyncRecord-icon {
+  width: 50px;
+  height: 50px;
+  font-size: 50px;
+}
+
+.TableSyncRecord-text {
+  width: 364px;
+  height: 20px;
+  font-size: 14px;
+  font-family: PingFangSC-Regular, PingFang SC;
+  font-weight: 400;
+  color: #4E4E4E;
+  line-height: 20px;
+  text-align: center;
+  margin-top: 16px;
+  margin-bottom: 20px;
+}
+
+.TableSyncRecord-customClass {
+    padding-bottom: 20px;
+    .el-message-box__header {
+      padding-top: 0;
+    }
+    .el-message-box__btns {
+      text-align: center;
+    }
+    .el-message-box__content {
+      .el-message-box__message {
+        padding-left: 0;
+      }
+      p {
+        font-size: 18px;
+        margin: 15px 0 10px;
+        text-align: center;
+      }
+      .el-icon-warning {
+        display: none;
+      }
+    }
+}
 </style>
--- src/modules/productsManagement/productsSync/components/ModalDataCompared.vue
+++ src/modules/productsManagement/productsSync/components/ModalDataCompared.vue
@@ -1,6 +1,48 @@
 <!--  -->
 <template>
-    <div></div>
+  <el-dialog
+    class="dialog-tight"
+    title="同步数据修改详情对比"
+    width="800px"
+    :visible.sync="visible"
+    append-to-body
+    center
+  >
+    <div style="padding-bottom:120px">
+      <div class="title flex align-c">
+          <div class="left flex align-c" style="margin-right:50px">
+            <el-image
+              style="height:50px;max-width:50px;border-radius:2px;margin-right:14px"
+              fit="contain"
+              :src="'https://img01.yzcdn.cn/upload_files/2017/06/19/Fme9JZz7T1rB8sSLnsnCk2gILNp_.png'"
+              :preview-src-list="['https://img01.yzcdn.cn/upload_files/2017/06/19/Fme9JZz7T1rB8sSLnsnCk2gILNp_.png']"
+              >
+              <div slot="placeholder">
+                  <hh-icon  type="iconwuzhaopian" style="font-size:50px" />
+              </div>
+              <div slot="error" class="flex align-c" style="height:100%">
+                  <hh-icon  type="icontupianjiazaishibai03" style="font-size:30px" />
+              </div>
+            </el-image>
+          <div>
+            <p class="color-4e font-12 mb-5">移动互联网产品设计的核心要素有哪些？</p>
+            <p class="color-999 font-12">商品ID：371073012830918</p>
+          </div>
+          </div>
+          <div class="right color-4e">
+            同步时间：2021-10-12 10:1
+          </div>
+      </div>
+
+      <el-table :data="tableData" style="width: 100%">
+        <el-table-column prop="date" label="日期" width="180" align="center">
+        </el-table-column>
+        <el-table-column prop="name" label="姓名" width="180" align="center">
+        </el-table-column>
+        <el-table-column prop="address" label="地址"> </el-table-column>
+      </el-table>
+    </div>
+  </el-dialog>
 </template>
 
 <script>
@@ -11,9 +53,43 @@ export default {
   },
   data () {
     return {
+      visible: false,
+      tableData: [
+        {
+          date: '2016-05-02',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄'
+        },
+        {
+          date: '2016-05-04',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1517 弄'
+        },
+        {
+          date: '2016-05-01',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1519 弄'
+        },
+        {
+          date: '2016-05-03',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1516 弄'
+        }
+      ]
+    }
+  },
+  methods: {
+    open () {
+      this.visible = true
     }
   }
 }
 </script>
 <style lang="less" scoped>
+.title {
+  width: 725px;
+  height: 50px;
+  background: #EAEDFA;
+  padding: 12px 16px;
+}
 </style>
