31a7b3edac8fb2cac3c1ea45c571850994ff59a4
songxue <songxue@meideng.net>
2021-09-08 17:23:04 +0800

    feat: 七天无理由退货

--- src/modules/migrate/migrateSettingTemplate/BasicTemplate.vue
+++ src/modules/migrate/migrateSettingTemplate/BasicTemplate.vue
@@ -67,7 +67,7 @@
             <el-form-item label="售后服务:"  prop="supply_7day_return">
                 <el-radio-group v-model="template.model.supply_7day_return">
                     <el-radio :label="1">支持7天无理由退换货</el-radio>
-                    <el-radio :label="2">支持(拆封后不支持)</el-radio>
+                    <el-radio :label="2">支持7天无理由退换货(拆封后不支持)</el-radio>
                     <el-radio :label="0">不支持7天无理由退换货</el-radio>
 <!--                    <el-radio :label="2">支持（拆封后不支持）</el-radio>-->
                 </el-radio-group>
