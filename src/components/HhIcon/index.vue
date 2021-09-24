85f0eb76ed5ed57262123a9e25f1a7a4667952eb
songxue <songxue@meideng.net>
2021-09-24 17:31:40 +0800

    feat: 商品源同步

--- src/components/HhIcon/index.vue
+++ src/components/HhIcon/index.vue
@@ -2,7 +2,7 @@
 import createFromIconfontCN from './createFromIconfontCN.js'
 
 const IconFont = createFromIconfontCN([
-  '//at.alicdn.com/t/font_2290850_zpbag2ujyje.js'
+  '//at.alicdn.com/t/font_2290850_jlykgnxx2lb.js'
 ])
 
 export default IconFont
--- src/components/Sidebar/index.vue
+++ src/components/Sidebar/index.vue
@@ -84,6 +84,12 @@
            <!-- <el-menu-item index="/shopDecorate/poster/dg/list" :disabled="!isAuth()">
              <span slot="title">批量新增详情</span>
            </el-menu-item> -->
+           <el-menu-item index="/productsManagement/productsSync" :disabled="!isAuth()">
+             <div slot="title">
+                <hh-icon type="iconshangpinyuantongbuweixuanzhong1" style="font-size:16px; margin-right:4px"></hh-icon>
+                <span class="">商品源同步</span>
+              </div>
+           </el-menu-item>
           </el-submenu>
           <el-submenu index="3">
              <template slot="title">
