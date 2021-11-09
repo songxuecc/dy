8b8dffacbe93a66068092fa8a371873b60fb2fb0
songxue <songxue@meideng.net>
2021-11-09 16:24:03 +0800

    feat: icon

--- src/components/HhIcon/index.vue
+++ src/components/HhIcon/index.vue
@@ -2,7 +2,7 @@
 import createFromIconfontCN from './createFromIconfontCN.js'
 
 const IconFont = createFromIconfontCN([
-  '//at.alicdn.com/t/font_2290850_z0mi9zrhni.js'
+  '//at.alicdn.com/t/font_2290850_2zroqj20mb1.js'
 ])
 
 export default IconFont
