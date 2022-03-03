520fa714b4a434b1f93f0324827ef5eb4f3e9754
songxue <songxue@meideng.net>
2022-03-03 23:48:23 +0800

    feat: 富豪

--- webpack/webpack.production.js
+++ webpack/webpack.production.js
@@ -18,7 +18,7 @@ const webpackPro = {
     path: path.resolve(__dirname, '../dist'),
     filename: 'static/js/[id].[chunkhash].js',
     chunkFilename: 'static/js/[id].[chunkhash].js',
-    publicPath: '`https://dycdn.huhuguanjia.com/'
+    publicPath: 'https://dycdn.huhuguanjia.com/'
   },
   plugins: [
     new CleanWebpackPlugin(),
