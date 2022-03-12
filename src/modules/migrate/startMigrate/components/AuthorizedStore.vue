34657c365ed1c65b03a641574843241c01dda969
songxue <songxue@meideng.net>
2022-03-12 10:53:43 +0800

    feat: 添加教程

--- src/modules/migrate/startMigrate/components/AuthorizedStore.vue
+++ src/modules/migrate/startMigrate/components/AuthorizedStore.vue
@@ -5,7 +5,7 @@
           <el-row class="flex mb-20" style="flex:1">
             <el-col :span="18" ><el-input v-model="tp_code" placeholder="您未绑定店铺，请先输入授权码，绑定其他平台店铺后，再进行复制。如何授权点击查看操作教程"></el-input></el-col>
             <el-col :span="3"><el-button class="ml-10" @click="handleBind" type="primary" style="width:120px">授权店铺</el-button></el-col>
-            <el-col :span="3"><el-button class="ml-10" @click="handleBind" type="warning" style="width:120px">查看教程</el-button></el-col>
+            <el-col :span="3"><el-button class="ml-10" @click="gotoShouquan" type="warning" style="width:120px">查看教程</el-button></el-col>
           </el-row>
         </div>
         <div v-if="tableData && tableData.length">
@@ -106,6 +106,9 @@ export default {
     },
     startCapture (row) {
       this.$emit('startCapture', row)
+    },
+    gotoShouquan () {
+      window.open('https://meideng.yuque.com/docs/share/730a3a18-0df7-4ea4-99a0-93f2c87e49c1?#')
     }
   }
 }
