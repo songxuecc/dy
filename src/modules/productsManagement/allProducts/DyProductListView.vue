a071869c415581904d9a2ad070cd8eafc47b248f
songxue <songxue@meideng.net>
2022-03-16 22:38:19 +0800

    feat: 货源链接

--- src/modules/productsManagement/allProducts/DyProductListView.vue
+++ src/modules/productsManagement/allProducts/DyProductListView.vue
@@ -77,9 +77,12 @@
                     {{ scope.row.min_price === scope.row.max_price ? '' : scope.row.max_price / 100 }}
                 </template>
             </el-table-column>
-            <el-table-column label="货源链接" width="80">
+            <el-table-column label="货源链接" width="120">
                 <template slot-scope="scope" v-if="scope.row.origin_url">
-                  <el-link type="primary" :underline="false" :href="scope.row.origin_url" target="view_window">货源链接</el-link>
+                  <div class="flex align-c">
+                    <el-link type="primary" :underline="false" :href="scope.row.origin_url" target="view_window" class="mr-5">货源链接</el-link>
+                    <hh-icon type="iconbianji-primary" @click="editOriginUrl(scope.row)"></hh-icon>
+                  </div>
                 </template>
             </el-table-column>
             <el-table-column prop="goods_quantity" label="库存" width="80">
@@ -196,9 +199,11 @@ import productOptimizeView from '@/components/ProductOptimizeView.vue'
 import request from '@/mixins/request.js'
 import common from '@/common/common.js'
 import utils from '@/common/utils.js'
+import services from '@services'
 import checkSyncProducts from '@/mixins/checkSyncProducts.js'
 
 export default {
+  inject: ['reload'],
   mixins: [
     request,
     checkSyncProducts('productsManagement')
@@ -498,6 +503,25 @@ export default {
           type: 'error'
         })
       }
+    },
+    editOriginUrl (row) {
+      this.$prompt('', '请输入货源链接', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消'
+      }).then(({ value }) => {
+        const parmas = {
+          goods_id: row.goods_id,
+          origin_url: value
+        }
+        services.produSourceUpdate(parmas).then(data => {
+          this.$message({
+            type: 'success',
+            message: '修改成功 '
+          })
+          this.reload()
+        })
+      }).catch(() => {
+      })
     }
   }
 }
